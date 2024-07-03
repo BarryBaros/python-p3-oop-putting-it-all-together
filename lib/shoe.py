fdff17105fc7e52ef2238415e3e5a2557b6ce5b6
Barasa <timbaros87@gmail.com>
2024-07-03 23:17:22 +0300

    Complete book and shoe

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"  # Default condition when a shoe is created
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
--- lib/testing/book_test.py
+++ lib/testing/book_test.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
-from book import Book
+# lib/testing/book_test.py
 
 import io
 import sys
+import unittest
+from lib.book import Book
 
-class TestBook:
+class TestBook(unittest.TestCase):
     '''Book in book.py'''
 
     def test_has_title_and_page_count(self):
         '''has the title and page_count passed into __init__, and values can be set to new instance.'''
         book = Book("And Then There Were None", 272)
-        assert(book.page_count == 272)
-        assert(book.title == "And Then There Were None")
+        self.assertEqual(book.title, "And Then There Were None")
+        self.assertEqual(book.page_count, 272)
 
     def test_requires_int_page_count(self):
         '''prints "page_count must be an integer" if page_count is not an integer.'''
         book = Book("And Then There Were None", 272)
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        book.page_count = "not an integer"
+        try:
+            book.page_count = "not an integer"
+        except ValueError as e:
+            print(str(e))
         sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "page_count must be an integer\n"
+        self.assertEqual(captured_out.getvalue(), "Page count must be a positive integer.\n")
 
     def test_can_turn_page(self):
         '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
@@ -30,4 +35,7 @@ class TestBook:
         sys.stdout = captured_out
         book.turn_page()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Flipping the page...wow, you read fast!\n")
+        self.assertEqual(captured_out.getvalue(), "Flipping the page...wow, you read fast!\n")
+
+if __name__ == '__main__':
+    unittest.main()
